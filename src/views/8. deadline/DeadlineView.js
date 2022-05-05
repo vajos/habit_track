e025d8136fd788c497fb943f024c5175d5860f74
vajos <vajos2@gmail.com>
2022-05-05 16:09:27 +0200

    added deadline

--- src/views/8. deadline/DeadlineView.js
+++ src/views/8. deadline/DeadlineView.js
@@ -1,16 +1,71 @@
+import {DatePicker, Select, Space, Switch, TimePicker} from 'antd';
+import {Option} from "antd/es/mentions";
+import Paragraph from "antd/es/typography/Paragraph";
+import moment from 'moment';
+
 import { useState } from "react";
-import { Alert, Button, Form, Input, message } from "antd";
-import auth0 from "../../config/auth0";
-import { Link, Routes, useLocation, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { updateUser, resetUser } from "./../../user/userReducer";
-import { RouteName } from "../../routes/routesnames";
+
+
 
 export default function DeadlineView() {
 
-  return (
-    <>
+    const [zieltermin, setZieltermin] = useState(false);
+    const format = 'HH:mm';
 
-    </>
-  );
+
+    function onChange(date, dateString) {
+        console.log(date, dateString);
+    }
+
+    function onSearch(val) {
+        console.log('search:', val);
+    }
+
+
+    function onChangeZieltermin(checked) {
+        console.log(`switch to ${checked}`);
+        setZieltermin(checked);
+        if (zieltermin) {
+            return <Paragraph>Anfangsdatum wählen</Paragraph>;
+        }
+    }
+
+    return (
+        <> <Space direction="vertical">
+
+            <Space direction="horizontal">
+                <Paragraph>Anfangsdatum wählen</Paragraph>
+                <DatePicker onChange={onChange}/>
+            </Space>
+
+            <Space direction="horizontal">
+                <Paragraph>Zieltermin</Paragraph>
+                <Switch onChange={onChangeZieltermin}/>
+            </Space>
+
+            <Space direction="horizontal">
+                <Paragraph>Erinnerungszeit</Paragraph>
+                <TimePicker defaultValue={moment('12:08', format)} format={format}/>
+            </Space>
+
+            <Space direction="horizontal">
+                <Paragraph>Priorität</Paragraph>
+                <Select
+                    showSearch
+                    placeholder="Priority"
+                    optionFilterProp="children"
+                    onChange={onChange}
+                    onSearch={onSearch}
+                    filterOption={(input, option) =>
+                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }
+                >
+                    <Select.Option value="1">1</Select.Option>
+                    <Select.Option value="2">2</Select.Option>
+                    <Select.Option value="3">3</Select.Option>
+                </Select>
+            </Space>
+        </Space>
+        </>
+    );
 }
