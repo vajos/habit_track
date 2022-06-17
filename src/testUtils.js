7033f598600c8d7deb878a553955214596b03c2d
Vaio <vajos2@gmail.com>
2022-06-17 13:29:45 +0200

    rfdsfds

--- src/testUtils.js
+++ src/testUtils.js
@@ -1,6 +1,4 @@
 /* istanbul ignore file */
-<<<<<<< HEAD
-import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "@emotion/react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
@@ -29,16 +27,4 @@ export const AllProviders = ({ children }) => (
         <BrowserRouter>{children}</BrowserRouter>
       </Provider>
     </ThemeProvider>
-  </ApolloProvider>
-=======
-import { ThemeProvider } from "@emotion/react";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import defaultTheme from "./layouts/themes/default";
-
-export const AllProviders = ({ children }) => (
-  <ThemeProvider theme={defaultTheme}>
-    <BrowserRouter>{children}</BrowserRouter>
-  </ThemeProvider>
->>>>>>> d193a2d53e810fa461db9f876c1eccbb90e7cf54
-);
+  </ApolloProvider>)
