f45eb7de4d2025b65de58adc0a20e54b1a053d9d
Vaio <vajos2@gmail.com>
2022-06-18 14:45:53 +0200

    fsdfds

--- src/views/1. login/__tests__/LoginView.test.js
+++ src/views/1. login/__tests__/LoginView.test.js
@@ -5,7 +5,8 @@ import { render, screen } from "@testing-library/react";
 import LoginRoutes from "../Login.routes";
 
 test("on intial render", () => {
-  render(<LoginRoutes />);
-
-  screen.debug();
+  //render(<App />);
+  //const el = screen.getByText(/Crap Stories/i);
+  //TODO hier ein test machen expect(el).toBeInTheDocument();
+  expect(3).toBe(3);
 });
