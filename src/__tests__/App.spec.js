17686b0a6fa4b53e803200f4f1fef4d97b85cb2d
AdisBabic <92baad1bif@hft-stuttgart.de>
2022-06-22 18:55:09 +0200

    app.js testet

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,12 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-
 describe("App", () => {
   it("should render a headline", () => {
-    //render(<App />);
-    //const el = screen.getByText(/Crap Stories/i);
-    //TODO hier ein test machen expect(el).toBeInTheDocument();
-    expect(3).toBe(3);
+    render(<App />);
   });
 });
