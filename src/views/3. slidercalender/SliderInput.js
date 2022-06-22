21d093aed8e2a7ad16e47c393b4f86c7daa6da5b
AdisBabic <92baad1bif@hft-stuttgart.de>
2022-06-22 15:43:38 +0200

    - add sliderinput

--- src/views/3. slidercalender/SliderInput.js
+++ src/views/3. slidercalender/SliderInput.js
@@ -18,6 +18,7 @@ export default function SliderInput({ plainOptions, date, marked }) {
   useEffect(() => {
     parseData();
   });
+
   function parseData() {
     marked.map((x) => {
       data.push(x);
